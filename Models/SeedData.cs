93dc382f1f8c6fb2f599329755e8ff167e1b3dc3
Vick Moberg <vicktor.moberg@gmail.com>
2020-10-28 10:47:30 -0400

    updated Seed Data

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -26,7 +26,17 @@ namespace RazorPagesMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "R"
+                    },
+
+                      new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -34,7 +44,8 @@ namespace RazorPagesMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -42,7 +53,8 @@ namespace RazorPagesMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating= "NA"
                     }
 
                 );
