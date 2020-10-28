e319121e3b430c489921835560ee04000f0a36c0
Vick Moberg <vicktor.moberg@gmail.com>
2020-10-28 09:17:48 -0400

    merged search branch

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -30,7 +30,7 @@ namespace RazorPagesMovie.Pages.Movies
 
         public async Task OnGetAsync()
         {
-            // Use LINQ to get list of fenres.
+            // Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Movie
                                             orderby m.Genre
                                             select m.Genre;
